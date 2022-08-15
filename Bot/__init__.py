61c0c4abc120fd6ede813aaaca198e7a136e34a7
Soheru San <contact.sohail.me@gmail.com>
2022-08-14 19:45:00 -0700

    done

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -19,7 +19,7 @@ BOT_NAME = os.environ.get('BOT_NAME', 'Soheru')
 #<---------------Connecting-------------->
 if BOT_TOKEN is not None:
     try:
-        encoder  = Client('AutoRename', api_id=API_ID, api_hash=APP_HASH, bot_token=BOT_TOKEN, plugins=dict(root="Bot/plugins"))
+        encoder  = Client('AutoEncoder', api_id=API_ID, api_hash=APP_HASH, bot_token=BOT_TOKEN, plugins=dict(root="Bot/plugins"))
         LOG.info('❤️ Bot Connected Created By Github @soheru || Telegram @sohailkhan_indianime ')
     except Exception as e:
         LOG.warn(f'😞 Error While Connecting To Bot\nCheck Errors: {e}')
--- Bot/plugins/encoder.py
+++ Bot/plugins/encoder.py
@@ -1,9 +1,10 @@
+from enum import auto
 import time, os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup as IKM, InlineKeyboardButton as IKB
 
 from Bot.plugins.database.mongo_db import check_user_mdb,check_vcodec_settings,update_vcodec_settings,check_preset_settings,update_preset_settings,check_resolution_settings,check_audio_type_mdb, update_audio_type_mdb,update_resolution_settings
-from Bot.utils.decorators import ffmpeg_auto_settings, ffmpeg_settings
+from Bot.utils.decorators import ffmpeg_settings
 from Bot import encoder, OWNER_ID, LOG, FILES_CHANNEL
 from Bot.utils.progress_pyro import progress_for_pyrogram
 from Bot.utils.ffmpeg import ffmpeg_progress
@@ -25,85 +26,36 @@ async def add_task(message):
         try:
             filepath = await message.download(progress=progress_for_pyrogram,progress_args=("**Downloading...**", msg, c_time))   
             check_resolution = check_resolution_settings(message.from_user.id)
-            print(check_resolution)
-            if check_resolution == 'auto':
-                cmd = ffmpeg_auto_settings(message.from_user.id, filepath, FT)
-                print(cmd)
-                await msg.edit_text('**Encoding...**')
-                try:
-                    lq = await ffmpeg_progress(cmd[0], filepath,f'progress-{FT}.txt',FT, msg, '**Encoding Started 480p**\n\n')
-                except Exception as e:
-                    LOG.info(f'ERror while ffmpeg progress\n' +e) 
-                try:
-                    mq = await ffmpeg_progress(cmd[1], filepath,f'progress-{FT}.txt',FT, msg, '**Encoding Started 720p**\n\n')
-                except Exception as e:
-                    LOG.info(f'ERror while ffmpeg progress\n' +e)     
-                try:
-                    hq = await ffmpeg_progress(cmd[2], filepath,f'progress-{FT}.txt',FT, msg, '**Encoding Started 1080p**\n\n')
-                except Exception as e:
-                    LOG.info(f'Error while ffmpeg progress\n' +e)     
-                output = filepath.rsplit('.',1)[0]
+            cmd = ffmpeg_settings(message.from_user.id, filepath, FT)  
+            await msg.edit_text('**Encoding...**')
+            try:
+                await ffmpeg_progress(cmd, filepath,f'progress-{FT}.txt',FT, msg, '**Encoding Started**\n\n')
+            except Exception as e:
+                LOG.info(f'ERror while ffmpeg progress\n' +e)  
+            output = filepath.rsplit('.',1)[0]
+            output = output+'_IA.mkv'     
+            file_name = filepath.rsplit('/', 1)
+            try: #MSG EDIT AND EDIT
+                await msg.edit(f'**Encoding Completed')   
+                file =  await msg.reply_document(output, caption=f"**{check_resolution}**", file_name=)  
+            except Exception as e: 
+                LOG.info(f'Error while file sending\n'+e)  
+            try:
+                await file.copy(FILES_CHANNEL)
+            except Exception as e: 
+                LOG.info(f'Error while file copy\n'+e)
                 
-                try: #MSG EDIT AND EDIT
-                    await msg.edit(f'**Encoding Completed')   
-                    lq =  await msg.reply_document(output+'480p.mkv', caption='480p')  
-                    mq =  await msg.reply_document(output+'720p.mkv', caption='720p')  
-                    hq =  await msg.reply_document(output+'1080p.mkv',caption='1080p')  
-                except Exception as e: 
-                    LOG.info(f'Error while file sending\n'+e)  
-                try:
-                    await lq.copy(FILES_CHANNEL)
-                    await mq.copy(FILES_CHANNEL)
-                    await hq.copy(FILES_CHANNEL)
-                except Exception as e: 
-                    LOG.info(f'Error while file copy\n'+e)
-                    
-                try: #FILE DELETE
-                    os.remove(filepath)
-                    os.remove(output+"480p".mkv)
-                    os.remove(output+"720p".mkv)
-                    os.remove(output+"1080p".mkv)
-                    os.remove(f'progress-{FT}.txt')
-                except Exception as e: 
-                    LOG.info(f'Error while removing files\n'+e)      
-            
-                try: #MSG DELETE
-                    await msg.delete() 
-                except:
-                    pass   
-                    
-            else:
-                cmd = ffmpeg_settings(message.from_user.id, filepath, FT)  
-                await msg.edit_text('**Encoding...**')
-                try:
-                    await ffmpeg_progress(cmd, filepath,f'progress-{FT}.txt',FT, msg, '**Encoding Started**\n\n')
-                except Exception as e:
-                    LOG.info(f'ERror while ffmpeg progress\n' +e) 
-                
-                output = filepath.rsplit('.',1)[0]
-                output = output+'_IA.mkv'     
-            
-                try: #MSG EDIT AND EDIT
-                    await msg.edit(f'**Encoding Completed')   
-                    file =  await msg.reply_document(output, caption=f"**{check_resolution}**")  
-                except Exception as e: 
-                    LOG.info(f'Error while file sending\n'+e)  
-                try:
-                    await file.copy(FILES_CHANNEL)
-                except Exception as e: 
-                    LOG.info(f'Error while file copy\n'+e)
-                
-                try: #FILE DELETE
-                    os.remove(filepath)
-                    os.remove(output)
-                    os.remove(f'progress-{FT}.txt')
-                except Exception as e: 
-                    LOG.info(f'Error while removing files\n'+e)      
+            try: #FILE DELETE
+                os.remove(filepath)
+                os.remove(output)
+                os.remove(f'progress-{FT}.txt')
+            except Exception as e: 
+                LOG.info(f'Error while removing files\n'+e)      
            
-                try: #MSG DELETE
-                    await msg.delete() 
-                except:
-                    pass       
+            try: #MSG DELETE
+                await msg.delete() 
+            except:
+                pass       
                        
         except Exception as e: 
             LOG.info(f'Error while line 56\n'+e)    
