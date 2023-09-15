61105d3ba624ada4f90e205a70f6671032afa9dc
Solo Leveling <109235836+SoloLeveling7seven@users.noreply.github.com>
2023-09-15 20:06:45 +0530

    Update __main__.py
    Signed-off-by: Solo Leveling <109235836+SoloLeveling7seven@users.noreply.github.com>

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,9 +1,13 @@
-from Bot import OWNER_ID, encoder
+from Bot import OWNER_ID, encoder, create_ubot
 from pyrogram.types import InlineKeyboardMarkup as IKM, InlineKeyboardButton as IKB
 import asyncio
 
 encoder.start()
 
+success = create_ubot()
+if success != None:
+    ubot = success
+    ubot.start()
 
 try:
     encoder.send_message(OWNER_ID, text='Bot Started', reply_markup=IKM([[IKB('ʜᴇʟᴘ', 'answer_help'), IKB('ᴅᴇᴠᴇʟᴏᴘᴇʀ', 'answer_about_dev')]]))
